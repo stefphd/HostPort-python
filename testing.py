fd8afcf4bcd1f26c90995e3f6b754ffb96d11c8d
stefphd <stefano.lovato.1@phd.unipd.it>
2022-06-27 19:52:10 +0200

    Create testing script

--- testing.py
+++ testing.py
@@ -4,39 +4,57 @@
 # Testing script for the Python binding of the HostPort class #
 #-------------------------------------------------------------#
 
+# Import the python modules
 import hostport
 import numpy as np
 
+# Variables
+port = 3 #serial port
+baud = 115200 #baudrate
+len = 64 #size of the buffer to read
+data = np.array([0, 1, 2, 3, 4, 5], dtype=np.single) #buffer to write
+
+# Instantiate a new object
 h = hostport.HostPort()
 
-h.begin(2, 115200)
+# Start the communication
+flag = h.begin(port, baud)
+# flag = h.begin(port, baud, header, terminator) #set also header and terminator
+# flag = h.begin(port, baud, header, terminator, timeout) #set also timeout
+
+# read 'len' elements of single, return true if success
+data_read = np.empty(len, dtype=np.single) #buffer must be defined as a numpy array
+flag = h.read(data_read)
+
+# write buffer (must be a numpy array)
+flag = h.write(data)
+
+# Check initialization
+if not h.isInit(): #or hostport.isInit or hostport.begin(port, baud)
+    print('Unable to connect')
+
+# Check read or write
+if not flag:
+    print('Unable to read or write')
+
+# Show all properties
 print(h)
 
-h.begin(2, 115200, 0xABCD1122, 0x1122DEFA)
-print(h)
-
-h.begin(2, 115200, 0xABCD1122, 0x1122DEFA, 500)
-print(h)
-
+# Set methods
 h.setPort(3)
 h.setBaud(9600)
 h.setHeader(0xABDE1234)
 h.setTerminator(0x1234ABCD)
+
+# Get methods
 print(h.getPort())
 print(h.getBaud())
 print(h.isInit())
 print(hex(h.getHeader()))
 print(hex(h.getTerminator()))
 
-h.close()
-print(h)
-
+# Restart
 h.restart()
-print(h)
 
-#HostPort::read(unsigned char* packetPtr, unsigned int size) 
-buf = np.empty(64, dtype=np.single)
-print(buf)
-h.read(buf)
-print(buf)
-h.write(buf)
+# Close
+h.close()
